c22cac725fb7b1807f3359b4435119986bcbbcde
bharath <bharathkumar.v@gofrugal.com>
2022-05-30 13:13:15 +0530

    inital commit started integration wetrack

---
+++ src/main/java/nr/king/familytracker/DemoApplication.java
@@ -0,0 +1,13 @@
+package nr.king.familytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
